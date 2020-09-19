a4be626fd464cb6487570c6e84c40b1e1b2b53dd
Pham, Son Tung <pham.sontung@hotmail.de>
2020-09-20 00:02:10 +0200

    add hex file for sin samples implemented read from hex the samples to SRAM

--- i2s_sinussignal_1.srcs/sources_1/imports/rtl/sd_audio.v
+++ i2s_sinussignal_1.srcs/sources_1/imports/rtl/sd_audio.v
@@ -5,24 +5,24 @@
 module sd_audio(
 					input  clk_50m,           //50Mhz clock input
 					input rst_n,
-					output SD_clk,
+					//output SD_clk,
 					
 					
 					input DACLRC,
-					input BCLK,
-					output DACDAT,
+					input BCLK, //BCLK posedge -> new DACDAT, else constant DACDAT
+					output DACDAT, // input for I2S DAC
 					
-					output I2C_SCLK,
-					inout I2C_SDAT
+					output I2C_SCLK, // to control I2C DAC
+					inout I2C_SDAT // serial data
 				   
 					
     );
 
-wire [15:0] wav_data = 16'hABCD;
+reg [15:0] wav_data = 16'hABCD;
 wire wav_rden;
 
 wire [31:0]read_sec;
-wire read_SD;
+//wire read_SD;
 
 wire [7:0]mydata_o;
 wire myvalid_o;
@@ -30,8 +30,31 @@ wire myvalid_o;
 wire [7:0]rx_o;
 wire init_o;
 wire read_o;
+reg [15:0] myram[43:0];
+reg [12:0] ram_raddr;
 
+initial $readmemh ("1kHz1ms.hex", myram);
 
+always @(posedge clk_50m)
+begin
+	if(!rst_n) begin
+		ram_raddr<=0;
+	end
+	else if(wav_rden) 
+    	if (ram_raddr>43)
+	      ram_raddr<=0;
+	    else
+	      ram_raddr<=ram_raddr+1;
+end
+
+//如果rden有效，16bit数据输出
+always @(posedge clk_50m)
+begin
+	if(wav_rden)
+	   wav_data<=myram[ram_raddr];	
+	else 
+	   wav_data<=wav_data;		
+end		
 
 //����wm8731�Ĳ���
 mywav	mywav_inst(
