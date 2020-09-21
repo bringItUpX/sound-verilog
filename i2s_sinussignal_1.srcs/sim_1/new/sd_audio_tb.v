94c05f992b3500ce943e19eefa8337757c0fc8ef
bringItUpX <brexter@gmx.de>
2020-09-21 23:41:05 +0200

    set reset delay to dec 160 (from hex ffff) only for simulating enhance testbench

--- i2s_sinussignal_1.srcs/sim_1/new/sd_audio_tb.v
+++ i2s_sinussignal_1.srcs/sim_1/new/sd_audio_tb.v
@@ -25,15 +25,13 @@ module sd_audio_tb(
     );
     reg clk_50m;           //50Mhz clock input
     reg rst_n;
-    reg SD_clk;
-    
-    
+
     reg DACLRC;
     reg BCLK;
-    reg DACDAT;
-    
-    reg I2C_SCLK;
-    reg I2C_SDA;
+    wire DACDAT;
+
+    wire I2C_SCLK;
+    wire I2C_SDAT;
 
 
 //  Unit under Test (UUT)
@@ -41,11 +39,36 @@ module sd_audio_tb(
 sd_audio uut(
     .clk_50m (clk_50m),
     .rst_n (rst_n),
-    .SD_clk (SD_clk),
     .DACLRC (DACLRC),
     .BCLK (BCLK),
     .DACDAT (DACDAT),
     .I2C_SCLK (I2C_SCLK),
-    .I2C_SDA (I2C_SDA));
+    .I2C_SDAT (I2C_SDAT));
     
+initial 
+   begin
+   clk_50m = 0;
+   BCLK = 0;
+   rst_n = 0; 
+   DACLRC = 0;  
+   end 
+
+   always
+   #1 clk_50m = ! clk_50m;
+     
+   always 
+   #10  BCLK =  ! BCLK;
+   
+   always 
+   #320  DACLRC = ! DACLRC;
+   
+initial
+   begin
+   #320;
+   rst_n = 1;
+   end
+
+initial
+
+   #5000000 $finish;
 endmodule
